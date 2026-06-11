849c5b9c2de44dab2f3294b953ec3eaf0215117f
jadalhassan <jadalhassan.ja034@gmail.com>
2026-06-11 16:41:28 +0300

    Runtime Error Fixing

--- src/main/java/com/blombank/cvautomation/CvAutomationApplication.java
+++ src/main/java/com/blombank/cvautomation/CvAutomationApplication.java
@@ -4,10 +4,8 @@ import com.blombank.cvautomation.services.EmailListenerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -16,9 +14,7 @@ public class CvAutomationApplication {
     private static final Logger logger = LoggerFactory.getLogger(CvAutomationApplication.class);
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(CvAutomationApplication.class, args);
-        int exitCode = SpringApplication.exit(context, (ExitCodeGenerator) () -> 0);
-        System.exit(exitCode);
+        SpringApplication.run(CvAutomationApplication.class, args);
     }
 
     @Bean
